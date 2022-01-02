b388f7a746c0bb3bf6d84b32c4143aa9ef5eaf01
George Middleton <georgemiddleton3@gmail.com>
2022-01-01 22:11:56 -0700

    Add invalid input/api error handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,18 @@ class HomeController < ApplicationController
     if params[:ticker] == ""
       redirect_to root_path, alert: 'You forgot to enter a symbol!'
     elsif params[:ticker]
-      @stock = StockQuote::Stock.quote(params[:ticker])
-      respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.update('ticker_data',
-                                                      partial: "home/ticker_data",
-                                                      locals: { stock: @stock })
+      puts params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+        respond_to do |format|
+          format.turbo_stream do
+            render turbo_stream: turbo_stream.update('ticker_data',
+                                                        partial: "home/ticker_data",
+                                                        locals: { stock: @stock })
+          end
         end
+      rescue
+        redirect_to root_path, alert: "Error occurred trying to get data for #{params[:ticker]}"
       end
     end
   end
