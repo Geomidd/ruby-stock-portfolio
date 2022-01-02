384a3682d5955261aa9f3f991291d555a181d679
George Middleton <georgemiddleton3@gmail.com>
2022-01-01 22:03:41 -0700

    Add empty input error handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: 'pk_b628a6d8440e4428979e4f4568ccf1e0')
-    if params[:ticker]
+    if params[:ticker] == ""
+      redirect_to root_path, alert: 'You forgot to enter a symbol!'
+    elsif params[:ticker]
       @stock = StockQuote::Stock.quote(params[:ticker])
       respond_to do |format|
         format.turbo_stream do
