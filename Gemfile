2c68df63afd7539d619446e901831bf93c2a4c7f
George Middleton <georgemiddleton3@gmail.com>
2022-01-02 14:14:24 -0700

    Add Devise gem and initial setup

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem "jbuilder"
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 gem 'stock_quote', '~> 3.0'
+gem 'devise', '~> 4.8', '>= 4.8.1'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/about'
+  devise_for :users
   root 'home#index'
-
+  
+  get 'home/about'
   post '/' => 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
