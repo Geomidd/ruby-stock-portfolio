b5bf657422c7711422eb38c8453a450ddd5a0429
George Middleton <georgemiddleton3@gmail.com>
2022-01-01 21:34:51 -0700

    Setup ticker search form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: 'pk_b628a6d8440e4428979e4f4568ccf1e0')
+    if params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update('ticker_data',
+                                                      partial: "home/ticker_data",
+                                                      locals: { stock: @stock })
+        end
+      end
+    end
   end
 end
