af7e545c95b4091b10be0c4b1312514bd2260b4a
George Middleton <georgemiddleton3@gmail.com>
2022-01-03 21:24:12 -0700

    Format stock partial and add stock data

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,2 +1,19 @@
 module StocksHelper
+  def get_stock_data(stock)
+    @stock_data = nil
+    @error = nil
+    if stock.ticker == ""
+      @error = "No ticker provided"
+    elsif stock.ticker
+      begin
+        @stock_data = StockQuote::Stock.quote(stock.ticker)
+      rescue
+        @error = "Could not retrieve data for #{stock.ticker}"
+      end
+    end
+    return {
+      'error' => @error,
+      'data' => @stock_data,
+    }
+  end
 end
