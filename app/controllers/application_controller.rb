4adb5ff795f630fa4e9dedc7a902e33aa4605626
George Middleton <georgemiddleton3@gmail.com>
2022-01-04 20:01:13 -0700

    Move StockQuote initialization to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  @api = StockQuote::Stock.new(api_key: 'pk_b628a6d8440e4428979e4f4568ccf1e0')
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @api = StockQuote::Stock.new(api_key: 'pk_b628a6d8440e4428979e4f4568ccf1e0')
     if params[:ticker] == ""
       redirect_to root_path, alert: 'You forgot to enter a symbol!'
     elsif params[:ticker]
